4a755a76cd1ad9d30923d4ed99af20a86d44e035
rebot <trenson.gilles@gmail.com>
2011-02-16 15:17:56 +0100

    ...

---
+++ myjavascript.js
@@ -0,0 +1,9 @@
+/* Deze functies dienen om de verschillende content weer te geven */
+
+function content1(){
+				document.getElementById('js-title').innerHTML ='<ul><h1>Titel 1</h1></ul>';
+			};
+			
+function content2(){
+				document.getElementById('js-title').innerHTML ='<ul><h1>Titel 2</h1></ul>';
+			};
