be378b23301affbb9588ba0f1e1dc0f78aec7da5
rebot <trenson.gilles@gmail.com>
2011-02-20 22:15:39 +0100

    mobile version under construction

---
+++ m/attributes/db.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost'; //usually localhost
+$username = 'rebot_inbox'; //your username assigned to your database
+$password = 'lolebroek'; //your password assigned to your user & database
+$database = 'rebot_inbox'; //your database name
+?>
